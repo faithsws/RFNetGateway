7e857c83332866cd8eee41176eda98d26d0fe2ad
unknown <songwensheng@songwensheng.dl-huachang.com.cn>
2013-07-24 13:19:22 +0800

    init

---
+++ misc.c
@@ -0,0 +1,38 @@
+void lcd_backlight_init()
+{}
+
+void lcd_backlight_on(unsigned char on)
+{}
+
+void timer1_init()
+{}
+void led_init()
+{}
+
+void led_r_on(unsigned char on)
+{}
+void led_g_on(unsigned char on)
+{}
+void led_y_on(unsigned char on)
+{}
+void delay1ms(unsigned short ms)
+{}
+void delay1us(unsigned short us)
+{}
+unsigned char get_r_led()
+{
+  return 1;
+}
+unsigned char get_g_led()
+{
+  return 1;
+}
+unsigned char get_y_led()
+{
+  return 1;
+}
+///////////////////////////////
+void lcd_print(char * str)
+{
+
+}
